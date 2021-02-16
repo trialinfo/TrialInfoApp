d8f02d91b8203ec2d0d496d6297937db9c5583c5
Andreas Gruenbacher <agruenba@redhat.com>
2021-02-16 03:23:25 +0100

    Add QR code scanning

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import * as dialogs from "tns-core-modules/ui/dialogs";
+import { BarcodeScanner } from 'nativescript-barcodescanner';
 
 import { SettingsService, DataService, SyncService, PollService, MySideDrawer } from "../shared";
+
+function parseQuery(queryString): any {
+    let args = {};
+    for (let arg of queryString.split('&')) {
+	if (arg == '')
+	    continue;
+	let match = arg.match(/^([^=]*)(?:=(.*))?/);
+	args[decodeURIComponent(match[1])] =
+	    match[2] == null ? true : decodeURIComponent(match[2]);
+    }
+    return args;
+}
 
 @Component({
     selector: "ns-login",
@@ -14,6 +27,7 @@ export class LoginComponent extends MySideDrawer implements OnInit {
     serverUrl: string;
     syncServerUrl: string;
     accessToken: string;
+    busy: boolean;
 
     constructor(
 	private settingsService: SettingsService,
@@ -30,8 +44,23 @@ export class LoginComponent extends MySideDrawer implements OnInit {
 	this.accessToken = this.settingsService.accessToken;
     }
 
+    async scan() {
+	let result = await new BarcodeScanner().scan({
+	    message: 'QR-Code aus TrialInfo-Einstellungen',
+	    resultDisplayDuration: 0
+	});
+	if (result.format == 'QR_CODE' && result.text && result.text.match(/^tr:/)) {
+	    let args = parseQuery(result.text.substr(3));
+	    this.serverUrl = (typeof args.url == 'string') ? args.url : null;
+	    this.syncServerUrl = (typeof args.sync == 'string') ? args.sync : null;
+	    this.accessToken = (typeof args.token == 'string') ? args.token : null;
+	    this.connect();
+	}
+    }
+
     async connect() {
 	try {
+	    this.busy = true;
 	    let connected = () => {
 		this.settingsService.serverUrl = this.serverUrl;
 		this.settingsService.syncServerUrl = this.syncServerUrl;
@@ -48,6 +77,8 @@ export class LoginComponent extends MySideDrawer implements OnInit {
 		okButtonText: 'OK'
 	    });
 	    return;
+	} finally {
+	    this.busy = false;
 	}
 	this.routerExtensions.navigate(['/register'], {clearHistory: true});
     }
